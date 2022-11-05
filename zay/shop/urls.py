abc42e9d780f94665c630b658fa644e05a09d43f
alesya <chibalice@yandex.by>
2022-11-05 22:23:31 +0300

    add commit

--- zay/shop/urls.py
+++ zay/shop/urls.py
@@ -1,10 +1,12 @@
+import contacts as contacts
 from django.urls import path, register_converter, re_path
 
-from .views import IndexView, about
+from .views import IndexView, about, CatalogListView
 from django.conf.urls import handler404
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('about/', about, name='about'),
+    path('shop', CatalogListView.as_view(), name='shop'),
 ]
--- zay/shop/views.py
+++ zay/shop/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 
 from .forms import ContactForm
-from .models import Category, Product
+from .models import Category, Product, Brand
 
 
 class IndexView(View):
@@ -37,40 +37,49 @@ class IndexView(View):
 def about(request: HttpRequest):
     brands = Brand.objects.all().order_by('name')
 
-    return render(request,
-                  'shop/about_us.html',
-                  {'brands': brands}
-                  )
+    return render(
+        request,
+        'shop/about.html',
+        {'brands': brands}
+        )
 
-class ProductView(ListView):
-    model = Category
-    template_name = 'shop/index.html'
+class ContextMixin:
+    context = {
+        'site_title': 'Zay',
+        'nav_home': 'Home',
+        'nav_about': 'About',
+        'nav_shop': 'Shop',
+        'nav_contact': 'Contact',
+        'shop_address': 'Svobody Street, 4, Minsk',
+        'shop_email': 'info@zay.com',
+        'shop_phone': '+375(33)-34-34-612',
+        'facebook': 'https://facebook.com',
+        'instagram': 'https://www.instagram.com/',
+        'twitter': 'https://twitter.com',
+        'linkedin': 'https://www.linkedin.com/',
+        'designed_by': 'https://templatemo.com/',
+    }
+class ShopMixin(ContextMixin):
+    context = ContextMixin.context
+    context.update({
+        'name': 'Categories',
+    })
+
+
+class CatalogListView(ShopMixin, ListView):
+    template_name = 'catalog/shop.html'
+    model = Product
     context_object_name = 'products'
-    object_list = None
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        content = super(ProductView, self).get_context_data()
-        categories = Category.objects.all().order_by('name')
-        content.update({
+    def get_context_data(self):
+        context = super(CatalogListView, self).get_context_data()
+        context.update(self.context)
+        categories = Category.objects.all()
+        context.update({
             'categories': categories,
-            'contact_form': ContactForm,
-            'contact_error': None
+            # 'subcategories': subcategories,
         })
-        return content
-
-    def get_queryset(self):
-        return Product.objects.filter(is_published=True).select_related('category')
-
-    def post(self, request):
-        form = ContactForm(request.POST)
-        content = self.get_context_data()
-        content['products'] = self.get_queryset()
-        if form.is_valid():
-            form.save()
-        else:
-            content['contact_error'] = 'error'
-        return render(request, self.template_name, content)
-
+        return context
 
 def error404(request, exception):
     return HttpResponse('<b>404<b>')
